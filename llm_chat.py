55eaea721e535444c8aea8d42408d3776958fbe8
harrychen1995 <harrychen1995@yahoo.com>
2025-01-31 09:21:29 -0600

    updated

--- llm_chat.py
+++ llm_chat.py
@@ -8,7 +8,7 @@ from web_automation import google_search, driver, go_to_web_page
 
 embeddings = OllamaEmbeddings(model="llama3.2")
 
-llm = ChatOllama(model="llama3.2", temperature= 100)
+llm = ChatOllama(model="llama3.2")
 
 
 
@@ -18,9 +18,9 @@ from langchain_core.tools import tool
 
 
 @tool
-def search_internet(query: str):
-    """serch internet with a query."""
-    urls = google_search(driver, query)
+def look_up_internet(topic: str):
+    """look up internet with a topic."""
+    urls = google_search(driver, topic)
     return '\n'.join(urls)
 
 
@@ -33,14 +33,14 @@ def summarize_web_content_from_link(link: str):
 # Step 1: Generate an AIMessage that may include a tool-call to be sent.
 def query_or_respond(state: MessagesState):
     """Generate tool call for retrieval or respond."""
-    llm_with_tools = llm.bind_tools([search_internet, summarize_web_content_from_link])
+    llm_with_tools = llm.bind_tools([look_up_internet, summarize_web_content_from_link]) if any(map(lambda x: x in state["messages"][-1].content.lower(), ["online", "search", "visit", "www"])) else llm
     response = llm_with_tools.invoke(state["messages"])
     # MessagesState appends messages to state instead of overwriting
     return {"messages": [response]}
 
 
 # Step 2: Execute the retrieval.
-search_internet_tool = ToolNode([search_internet], name = "search_internet_tool")
+look_up_internet_tool = ToolNode([look_up_internet], name = "look_up_internet_tool")
 summarize_web_content_from_link_tool = ToolNode([summarize_web_content_from_link], name = "summarize_web_content_from_link_tool")
 
 # Step 3: Generate a response using the retrieved content.
@@ -58,7 +58,7 @@ def generate_search_response(state: MessagesState):
     system_message_content = ''
     if tool_messages:
         
-        system_message_content = "your google search agent, after searching internet summary in url" + tool_messages[0].content + "in a table, please response in markdown format"
+        system_message_content = "your google search agent, base on search url results " +  "\n".join([tool_message.content for tool_message in tool_messages]) + "in a table, please response in markdown format"
     
     conversation_messages = [
         message
@@ -91,7 +91,7 @@ def generate_visit_link_response(state: MessagesState):
     system_message_content = ''
     if tool_messages:
         
-        system_message_content = "your google search agent, summarize web content" +  tool_messages[0].content + "in a table, please response in markdown format"
+        system_message_content = "your google search agent, summarize web content" +  "\n".join([tool_message.content for tool_message in tool_messages]) + "in a table, please response in markdown format"
     
     conversation_messages = [
         message
@@ -124,7 +124,7 @@ def route_by_message_state(state: MessagesState):
 graph_builder = StateGraph(MessagesState)
 
 graph_builder.add_node(query_or_respond)
-graph_builder.add_node(search_internet_tool)
+graph_builder.add_node(look_up_internet_tool)
 graph_builder.add_node(summarize_web_content_from_link_tool)
 graph_builder.add_node(generate_search_response)
 graph_builder.add_node(generate_visit_link_response)
@@ -133,9 +133,9 @@ graph_builder.set_entry_point("query_or_respond")
 graph_builder.add_conditional_edges(
     "query_or_respond",
     route_by_message_state,
-    {"END": END, "search_internet": "search_internet_tool", "summarize_web_content_from_link":"summarize_web_content_from_link_tool"},
+    {"END": END, "look_up_internet": "look_up_internet_tool", "summarize_web_content_from_link":"summarize_web_content_from_link_tool"},
 )
-graph_builder.add_edge("search_internet_tool", "generate_search_response")
+graph_builder.add_edge("look_up_internet_tool", "generate_search_response")
 graph_builder.add_edge("summarize_web_content_from_link_tool", "generate_visit_link_response")
 graph_builder.add_edge("generate_search_response", END)
 graph_builder.add_edge("generate_visit_link_response", END)
